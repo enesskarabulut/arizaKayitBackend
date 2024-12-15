8074b521d6f37636606eb42fefb1c92eeafc5337
Eness07 <147343980+Eness07@users.noreply.github.com>
2024-12-16 01:15:22 +0300

    fix

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS Middleware'i ekle
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://ariza-kayit-frontend.vercel.app'],
+  origin: ['http://localhost:3000', 'https://ariza-kayit-backend.vercel.app/api/auth'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
